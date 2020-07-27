7e69c557cb8c4b68a603f08a52c65fff2560e215
Jay Soni <jbsoni@uwaterloo.ca>
2020-07-27 12:58:15 -0400

    completed plotting functionality, there seems to be something wrong with state intialization, when measuring the qbits without applying any gates we get the states |1>

--- qsim.py
+++ qsim.py
@@ -1,26 +1,30 @@
 # Main file for the simulator
 import numpy as np
 from numpy import linalg as la
+import matplotlib.pyplot as plt
+from math import isclose
 
 
 def split_state(vect):
     assert (len(vect) == 4)
     qbit1 = Qbit(vect[0] + vect[1], vect[2] + vect[3])
     qbit2 = Qbit(vect[0] + vect[2], vect[1] + vect[3])
-    return qbit1, qbit2
+    return qbit1.state, qbit2.state
+
+
+def lst_to_str(lst):
+    result = ''
+    for i in lst:
+        result += str(int(i))  # sometimes i is a float
+    return result
 
 
 class Qbit:
 
     def __init__(self, c1, c2):
-        try:
-            assert (la.norm(c1)**2 + la.norm(c2)**2 == 1)
-        except AssertionError:
-            c1 = 1
-            c2 = 0
-            pass
-
+        assert isclose((np.sqrt(c1**2 + c2**2)), 1.0, abs_tol=1e-5)
         self.state = np.array([c1, c2])
+        return
 
     def __repr__(self):
         return "{0}|0> + {1}|1>".format(self.state[0], self.state[1])
@@ -32,6 +36,23 @@ class Qbit:
 
 
 class Circuit:
+
+    h_mat = 1 / np.sqrt(2) * np.array([[1,  1],
+                                       [1, -1]])
+
+    x_mat = np.array([[0, 1],
+                      [1, 0]])
+
+    y_mat = np.array([[0, complex(0, 1)],
+                      [complex(0, 1), 0]])
+
+    z_mat = np.array([[1, 0],
+                      [0, -1]])
+
+    gate_to_mat = {'H': h_mat,
+                   'X': x_mat,
+                   'Y': y_mat,
+                   'Z': z_mat}
 
     def __init__(self, num_qbits, num_bits):
         assert(num_qbits > 0)
@@ -49,9 +70,10 @@ class Circuit:
         disp_circuit = ''
         for i in range(self.num_qbits):
             row = 'q{}: |0>'.format(i)
-            for gate_element in self.gate_array[i]:
-                row += '--{0}'.format(gate_element)
-            row += '\n'
+            for meta_tuple in self.gate_array[i]:
+                gate_str = meta_tuple[0]
+                row += '--{0}'.format(gate_str)
+            row += '--M\n'
             disp_circuit += row
         return disp_circuit
 
@@ -63,19 +85,21 @@ class Circuit:
 
         for index, gate_lst in enumerate(self.gate_array):
             if index in qbit_ind:
-                gate_lst.append(gate_str)
+                meta_tuple = (gate_str, index)
             else:
-                gate_lst.append('-')
+                meta_tuple = ('-', index)
+            gate_lst.append(meta_tuple)
         return
 
     def add_dq_gate(self, control_ind, target_ind, gate_str):
         for index, gate_lst in enumerate(self.gate_array):
             if index == control_ind:
-                gate_lst.append('CQ')
+                meta_tuple = ('CQ', control_ind, target_ind, gate_str)
             elif index == target_ind:
-                gate_lst.append('T' + gate_str)
+                meta_tuple = ('T' + gate_str, control_ind, target_ind, gate_str)
             else:
-                gate_lst.append('-')
+                meta_tuple = ('-', index)
+            gate_lst.append(meta_tuple)
         return
 
     @staticmethod
@@ -93,106 +117,100 @@ class Circuit:
         control_qbit.state, target_qbit.state = split_state(resultant_state)
         return
 
-    def n(self, qbit_ind):
-        self.add_sq_gate(qbit_ind, 'N')
+    @staticmethod
+    def apply_gate(qbit, unitary_mat):
+        qbit.state = unitary_mat @ qbit.state
         return
+
+    def measure(self, qbit_lst, bit_lst, trails=100):
+        assert len(qbit_lst) == len(bit_lst)
+        assert len(bit_lst) == self.num_bits
+
+        circ_depth = len(self.gate_array[0])
+        for layer in range(circ_depth):
+            for index, qbit in enumerate(self.lst_qbits):
+                meta_tuple = self.gate_array[index][layer]
+                gate_str = meta_tuple[0]
+
+                if gate_str == '-' or gate_str[0] == 'T':
+                    pass
+
+                elif gate_str == 'CQ':
+                    target_mat = self.gate_to_mat[meta_tuple[3]]
+                    target_qbit = self.lst_qbits[meta_tuple[2]]
+                    control_qbit = qbit
+
+                    self.apply_controlgate(control_qbit, target_qbit, target_mat)
+
+                else:
+                    unitary_mat = self.gate_to_mat[gate_str]
+                    self.apply_gate(qbit, unitary_mat)
+
+        results_lst = []
+        for qbit in self.lst_qbits:
+            result = qbit.measure(shots=trails)
+            results_lst.append(result)
+
+        final_results_lst = np.zeros((self.num_bits, trails))
+        for i, j in zip(qbit_lst, bit_lst):
+            final_results_lst[j] = results_lst[i]
+
+        final_results_lst = np.transpose(final_results_lst)
+        counts_dict = {}
+        for row in final_results_lst:
+            state_label = lst_to_str(row)
+            if state_label in counts_dict:
+                counts_dict[state_label] += 1
+            else:
+                counts_dict[state_label] = 1
+
+        return counts_dict
 
     @staticmethod
-    def apply_n(qbit=Qbit(1, 0)):
-        not_mat = np.array([[0, 1],
-                            [1, 0]])
-        qbit.state = not_mat @ qbit.state
-        return not_mat
-
-    def cn(self, control_ind, target_ind):
-        self.add_dq_gate(control_ind, target_ind, 'N')
-        return
-
-    def apply_cn(self, control_qbit, target_qbit):
-        self.apply_controlgate(control_qbit, target_qbit, self.apply_n())
+    def plot_counts(counts):
+        plt.bar(range(len(counts)), counts.values())
+        plt.xticks(range(len(counts)), list(counts.keys()))
+        plt.show()
         return
 
     def h(self, qbit_ind):
         self.add_sq_gate(qbit_ind, 'H')
         return
 
-    @staticmethod
-    def apply_h(qbit=Qbit(1, 0)):
-        hadamard_mat = 1 / np.sqrt(2) * np.array([[1,  1],
-                                                  [1, -1]])
-        qbit.state = hadamard_mat @ qbit.state
-        return hadamard_mat
-
     def ch(self, control_ind, target_ind):
         self.add_dq_gate(control_ind, target_ind, 'H')
-        return
-
-    def apply_ch(self, control_qbit, target_qbit):
-        self.apply_controlgate(control_qbit, target_qbit, self.apply_h())
         return
 
     def x(self, qbit_ind):
         self.add_sq_gate(qbit_ind, 'X')
         return
 
-    @staticmethod
-    def apply_x(qbit=Qbit(1, 0)):
-        x_mat = np.array([[0, 1],
-                          [1, 0]])
-
-        qbit.state = x_mat @ qbit.state
-        return x_mat
-
     def cx(self, control_ind, target_ind):
         self.add_dq_gate(control_ind, target_ind, 'X')
-        return
-
-    def apply_cx(self, control_qbit, target_qbit):
-        self.apply_controlgate(control_qbit, target_qbit, self.apply_x())
         return
 
     def y(self, qbit_ind):
         self.add_sq_gate(qbit_ind, 'Y')
         return
 
-    @staticmethod
-    def apply_y(qbit=Qbit(1, 0)):
-        y_mat = np.array([[0, -j],
-                          [j, 0]])
-
-        qbit.state = y_mat @ qbit.state
-        return y_mat
-
     def cy(self, control_ind, target_ind):
         self.add_dq_gate(control_ind, target_ind, 'Y')
-        return
-
-    def apply_cy(self, control_qbit, target_qbit):
-        self.apply_controlgate(control_qbit, target_qbit, self.apply_y())
         return
 
     def z(self, qbit_ind):
         self.add_sq_gate(qbit_ind, 'Z')
         return
 
-    @staticmethod
-    def apply_z(qbit=Qbit(1, 0)):
-        z_mat = np.array([[1, 0],
-                          [0, -1]])
-
-        qbit.state = z_mat @ qbit.state
-        return z_mat
-
     def cz(self, control_ind, target_ind):
         self.add_dq_gate(control_ind, target_ind, 'Z')
         return
 
-    def apply_cz(self, control_qbit, target_qbit):
-        self.apply_controlgate(control_qbit, target_qbit, self.apply_z())
-        return
-
 
 def main():
+    circ = Circuit(2, 2)
+    print(circ)
+    counts = circ.measure([0, 1], [0, 1], trails=1024)
+    circ.plot_counts(counts)
     return
 
 
