a6bb5f6412fcdc3d1fed040d04b840f062a2e08f
Jay Soni <jbsoni@uwaterloo.ca>
2020-08-25 23:13:54 -0400

    Fixed implementation of binary iterator for the Control - Unitary gate Need to:    - change implementation of plotting the circuit to remove redundancy      in the control and target display    - fix implementation of the measure function

--- qsim.py
+++ qsim.py
@@ -6,43 +6,40 @@ from math import isclose
 import itertools
 
 
-def q_bit_ittorator(bit1, bit2, n):
+def binary_ittorator(control_bit, target_bit, n):
     """
-    Given two tuples with element < n, create all possible binary integers
-    of bit length n with the known bit1, bit2 positions. Return list of
-    ints that represent the base 10 of those binary numbers
+    create all possible binary integers of bit length n with the known control_bit, target_bit positions
+    held constant. Return list of all binary variations (as strs)
 
-    :param bit1: tuple - (element, bool)
-    :param bit2: tuple - (element, bool)
+    :param control_bit: int
+    :param target_bit: int
     :param n: int
-    :return: list of ints
+    :return: list of str
     """
     try:
-        assert(n > bit1[0])
-        assert(n > bit2[0])
+        assert(n > control_bit)
+        assert(n > target_bit)
     except AssertionError as e:
         print(f"Error: n ({n}) must be larger than the index of each bit")
         print(e)
     else:
         # itertools creates all possible binary combinations for n bites
         binary_combinations = [list(i) for i in itertools.product([0, 1], repeat=n)]
-        return_list = []  # Create an empty list to be populated with all possible ints
+        return_list = []  # Create an empty list to be populated
 
-        print(binary_combinations)  # For debuging only
         # For all elements, replace the bit1 element to bit1 value, same for bit2
         for i in range(len(binary_combinations)):
-            binary_combinations[i][bit1[0]] = int(bit1[1])
-            binary_combinations[i][bit2[0]] = int(bit2[1])
+            binary_combinations[i][control_bit] = 1
+            binary_combinations[i][target_bit] = 0
 
             # Convert each list of ints (1s & 0s) to a list of strings
             strings = [str(i) for i in binary_combinations[i]]
-            thing =  "".join(strings)
+            bin_string = "".join(strings)
 
             # Only add to the return list if it's unique
-            if as_a_int not in return_list:
-                return_list.append(as_a_int)
+            if bin_string not in return_list:
+                return_list.append(bin_string)
 
-        print(binary_combinations)  # For debuging only
         return return_list
 
 
